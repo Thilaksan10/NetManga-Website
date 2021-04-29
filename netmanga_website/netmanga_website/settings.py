13549c90994d6fe519d063b553331dc09804683a
Thilaksan10 <thilaksan.kodeeswaran@tu-dortmund.de>
2021-04-29 23:42:14 +0200

    change STATICFILE_STORAGE

--- netmanga_website/netmanga_website/settings.py
+++ netmanga_website/netmanga_website/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Simplifies static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'netmanga_website.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
