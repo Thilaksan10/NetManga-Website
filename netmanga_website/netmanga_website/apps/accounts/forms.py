5fa5f52115ac6a1acc10d3ca00b543b774d78a25
Thilaksan10 <thilaksan.kodeeswaran@tu-dortmund.de>
2021-04-30 19:22:52 +0200

    delete min_length in Manga_Form

--- netmanga_website/netmanga_website/apps/accounts/forms.py
+++ netmanga_website/netmanga_website/apps/accounts/forms.py
@@ -179,7 +179,6 @@ class MangaForm(forms.ModelForm):
    title = forms.CharField(
       required=True, 
       max_length=100,
-      min_lenght=1, 
       widget=forms.TextInput(
          attrs={
             'class': 'form-control',
