b66cb2c4e9c68a977d7320be4cd4eeed12f06772
Thilaksan10 <thilaksan.kodeeswaran@tu-dortmund.de>
2021-06-25 18:08:36 +0200

    'logging'

--- netmanga_website/manage.py
+++ netmanga_website/manage.py
@@ -8,6 +8,7 @@ import logging
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netmanga_website.settings')
+    logging.info('settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
