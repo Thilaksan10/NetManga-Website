5a4497ca66f2d6bc2d91ecfdf63f9cb312e8ea42
Thilaksan10 <thilaksan.kodeeswaran@tu-dortmund.de>
2021-07-03 17:19:37 +0200

    remove typo

--- netmanga_website/netmanga_website/settings.py
+++ netmanga_website/netmanga_website/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'log_request_id.middleware.ReuquestIDMiddleware',
+    'log_request_id.middleware.RequestIDMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
