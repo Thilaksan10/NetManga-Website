0741aadc6be3b766a225abfeda8f68dd4101e564
Thilaksan10 <thilaksan.kodeeswaran@tu-dortmund.de>
2021-07-09 16:02:39 +0200

    t

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,4 +4,15 @@ set -e
 
 python manage.py collectstatic --noinput
 
-uwsgi --http-socket :$PORT --buffer-size=32768 --master --enable-threads --module netmanga_website.wsgi
+uwsgi --chdir=./netmanga_website \ 
+    --module=netmanga_website.wsgi:application \ 
+    --env DJANGO_SETTINGS_MODULE=mysite.settings \ 
+    --master --pidfile=/tmp/project-master.pid \
+    --socket=127.0.0.1:$PORT \
+    --processes=5 \
+    --uid=1000 --gid=2000 \
+    --harakiri=20 \
+    --max-requests=5000 \
+    --vacuum \
+    --home=/path/to/virtual/env \
+    --daemonize=/var/log/uwsgi/netmanga.log
