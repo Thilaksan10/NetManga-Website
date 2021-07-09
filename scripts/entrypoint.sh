6bf6601da67302b845831d73c4965c82c7e284e6
Thilaksan10 <thilaksan.kodeeswaran@tu-dortmund.de>
2021-07-09 16:26:45 +0200

    t

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -8,7 +8,7 @@ uwsgi --chdir=./netmanga_website \
     --module=netmanga_website.wsgi:application \ 
     --env DJANGO_SETTINGS_MODULE=mysite.settings \ 
     --master --pidfile=/tmp/project-master.pid \
-    --socket=127.0.0.1:$PORT \
+    --http-socket=127.0.0.1:$PORT \
     --processes=5 \
     --uid=1000 --gid=2000 \
     --harakiri=20 \
