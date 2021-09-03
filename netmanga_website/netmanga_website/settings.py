c9c17d9f06a05a08d88faa34dc4f6ae2965fecce
Thilaksan10 <thilaksan.kodeeswaran@tu-dortmund.de>
2021-09-03 23:15:41 +0200

    Content Security Policy for Header

--- netmanga_website/netmanga_website/settings.py
+++ netmanga_website/netmanga_website/settings.py
@@ -66,8 +66,10 @@ SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_NAME = '__Secure-sessionid'
+CSRF_COOKIE_NAME = '__Secure-csrftoken'
 
-CSP_DEFAULT_SRC = ("'self'", "'unsafe-inline'")
+CSP_DEFAULT_SRC = ("'self'",)
 CSP_STYLE_SRC = ("'self'", "https://netmanga.s3.amazonaws.com/css/", "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css")
 CSP_SCRIPT_SRC = ("'self'", "https://netmanga.s3.amazonaws.com/js/", "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.1.1/chart.min.js", "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js")
 CSP_IMG_SRC = ("'self'", "https://netmanga.s3.amazonaws.com/img/", "https://netmanga.s3.amazonaws.com/")
