4e2c2a04ae6db3fd1c8ee8be38806139d77ba972
Thilaksan10 <thilaksan.kodeeswaran@tu-dortmund.de>
2021-09-03 21:17:35 +0200

    Content Security Policy for Header

--- netmanga_website/netmanga_website/settings.py
+++ netmanga_website/netmanga_website/settings.py
@@ -70,7 +70,7 @@ CSP_DEFAULT_SRC = ("'self'",)
 CSP_STYLE_SRC = ("'self'", "https://netmanga.s3.amazonaws.com/css/", "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css")
 CSP_SCRIPT_SRC = ("'self'", "https://netmanga.s3.amazonaws.com/js/", "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.1.1/chart.min.js", "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js")
 CSP_IMG_SRC = ("'self'", "https://netmanga.s3.amazonaws.com/img/", "https://netmanga.s3.amazonaws.com/")
-CSP_FONT_SRC = ("'self'", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.svg")
+CSP_FONT_SRC = ("'self'", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/")
 CSP_MEDIA_SRC = ("'self'", "https://netmanga.s3.amazonaws.com/")
 
 ROOT_URLCONF = 'netmanga_website.urls'
