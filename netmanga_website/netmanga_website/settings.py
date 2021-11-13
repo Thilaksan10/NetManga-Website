b41be138a77c706c4d1e72fe454cabcbdf617ee1
Thilaksan10 <thilaksan.kodeeswaran@tu-dortmund.de>
2021-11-13 21:56:25 +0100

    image url in chapter viewer test

--- netmanga_website/netmanga_website/settings.py
+++ netmanga_website/netmanga_website/settings.py
@@ -214,7 +214,7 @@ AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Simplifies static file serving.
 # https://warehouse.python.org/project/whitenoise/
