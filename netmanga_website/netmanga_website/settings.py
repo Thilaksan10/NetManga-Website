2ff7ece79f358ae5bcbd574309704a06e6ca934c
Thilaksan10 <thilaksan.kodeeswaran@tu-dortmund.de>
2021-11-15 19:07:47 +0100

    remove 'django.contrib.staticfiles', to installed Apps

--- netmanga_website/netmanga_website/settings.py
+++ netmanga_website/netmanga_website/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.staticfiles',
     'netmanga_website.apps.accounts',
     'netmanga_website.apps.help',
     'netmanga_website.apps.public',
